e31871801550174fef9691a5b308877d0a24e90f
IMTIAZ005 <iussikder@gmail.com>
2021-01-20 01:21:43 -0500

    This is a Demo Project created in Intellij

---
+++ src/pkg/demoproject/LoginTest.java
@@ -0,0 +1,8 @@
+package pkg.demoproject;
+
+public class LoginTest {
+
+    public static void main(String[] args) {
+        System.out.println("This is my first Demo Project in Intellij");
+    }
+}
